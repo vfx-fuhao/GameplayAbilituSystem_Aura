31bb261978b63a74dce45575ca6047d60dbcb3a7
Hal <vfx_fuhao@163.com>
2024-07-27 14:26:31 +0800

    create base character

---
+++ Source/AuraRPG/AuraRPG.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AuraRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AuraRPG, "AuraRPG" );
